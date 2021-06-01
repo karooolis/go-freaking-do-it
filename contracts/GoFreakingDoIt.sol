1c8ad388fea2cc5a736268ec2fa4b0ac10087722
Karolis Ramanauskas <karolis@uber.com>
2021-06-01 23:08:33 +0300

    Update GoFreakingDoIt.sol

--- contracts/GoFreakingDoIt.sol
+++ contracts/GoFreakingDoIt.sol
@@ -1,119 +1,103 @@
-pragma solidity ^0.8.4;
+pragma solidity ^0.4.15;
 
-import "https://github.com/smartcontractkit/chainlink/blob/master/evm-contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
 
-contract Bet {
-    address payable owner;
-
-    AggregatorV3Interface internal priceFeed;
-
-    // Bet status
-    enum BetStatus { LONG, SHORT, NGMI }
-    
-    //the game structure
-    struct Game {
-        uint256 betAmount;
-        uint256 guess;
-        BetStatus status;
-        address maker;
-        address taker;
+contract GoFreakingDoIt is Ownable {
+    struct Goal {
+        bytes32 hash;
+        address owner;
+        string description;
+        uint amount;
+        string supervisorEmail;
+        string creatorEmail;
         string deadline;
+        bool emailSent;
+        bool completed;
     }
 
-    modifier onlyOwner() {
-      require(msg.sender == owner);
-      _;
-    }
+    mapping (bytes32 => Goal) public goals;
+    Goal[] public activeGoals;
 
-    receive() external payable {
-    }
-
-     /**
-     * Network: Kovan
-     * Aggregator: ETH/USD
-     * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
-     */
-    constructor() public payable {
-      priceFeed = AggregatorV3Interface(address(0x9326BFA02ADD2366b30bacB125260Af641031331));
-    }
-
-    function getThePrice() public view returns (int) {
-      (
-        uint80 roundID,
-        int price,
-        uint startedAt,
-        uint timeStamp,
-        uint80 answeredInRound
-      ) = priceFeed.latestRoundData();
-
-      return price;
-    }
-    
-     event GameCreated (
-        uint newGameIndex,
-        uint amountBetted,
-        uint guess,
-        address by
+    event setGoalEvent (
+        address _owner,
+        string _description,
+        uint _amount,
+        string _supervisorEmail,
+        string _creatorEmail,
+        string _deadline,
+        bool _emailSent,
+        bool _completed
     );
-    
-    mapping (uint256 => Game) public activeGames;
 
-    function makeBet(uint _guess, string memory deadline) public payable returns (bytes32) {
-      Game memory newGame = Game(msg.value, _guess, BetStatus(0), msg.sender, address(0), deadline);
-      uint current_price = uint(getThePrice());
-      
-      if (_guess > current_price) {
-        newGame.status = BetStatus.LONG;
-      } else if (_guess < current_price) {
-        newGame.status = BetStatus.SHORT;
-      } else {
-        newGame.status = BetStatus.NGMI;
-      }
-      
-      bytes32 hash = keccak256(abi.encodePacked(msg.sender, msg.value, _guess, deadline, newGame.status));
-      activeGames[uint256(hash)] = newGame;
-      // activeGames.push(newGame);
-    
-      // index = index + 1;
-      // activeGames.push(newGame);
-      // emit GameCreated(newGame.index, msg.value, _guess, msg.sender);
-      
-      return hash;
+    event setGoalSucceededEvent(bytes32 hash, bool _completed);
+    event setGoalFailedEvent(bytes32 hash, bool _completed);
+
+    function setGoal(string _description, string _supervisorEmail, string _creatorEmail, string _deadline) public payable returns (bytes32, address, string, uint, string, string, string) {
+        require(msg.value > 0);
+        require(keccak256(_description) != keccak256(""));
+        require(keccak256(_creatorEmail) != keccak256(""));
+        require(keccak256(_deadline) != keccak256(""));
+
+        bytes32 hash = keccak256(msg.sender, _description, msg.value, _deadline);
+
+        assert(keccak256(goals[hash].description) == keccak256(""));
+
+        Goal memory goal = Goal({
+            hash: hash,
+            owner: msg.sender,
+            description: _description,
+            amount: msg.value,
+            supervisorEmail: _supervisorEmail,
+            creatorEmail: _creatorEmail,
+            deadline: _deadline,
+            emailSent: false,
+            completed: false
+        });
+
+        goals[hash] = goal;
+        activeGoals.push(goal);
+
+        setGoalEvent(goal.owner, goal.description, goal.amount, goal.supervisorEmail, goal.creatorEmail, goal.deadline, goal.emailSent, goal.completed);
+
+        return (hash, goal.owner, goal.description, goal.amount, goal.supervisorEmail, goal.creatorEmail, goal.deadline);
     }
 
-    function takeBet(uint256 _hash) public payable {
-        //requires the taker to make the same bet amount
-        Game memory game = activeGames[_hash];
-        
-        require(msg.value == game.betAmount);
-        
-        game.taker = msg.sender;
+    function getGoalsCount() public constant returns (uint count) {
+        return activeGoals.length;
     }
 
-    function getBetOutcome(uint256 _hash) public payable {
-      // TODO: remove game from activeGames map here
-      // TODO: insert asserts
+    function setEmailSent(uint _index, bytes32 _hash) public onlyOwner {
+        assert(goals[_hash].amount > 0);
 
-      uint current_price = uint(getThePrice());
-      Game memory activeGame = activeGames[_hash];
-      
-      if (activeGame.status == BetStatus(0) && current_price >= activeGame.guess) {
-        (bool sent, bytes memory data) = activeGame.maker.call{value: address(this).balance}("");
-        require(sent, "Failed to send Ether");
-        
-      } else if (activeGame.status == BetStatus(1) && current_price <= activeGame.guess) {
-          
-        (bool sent, bytes memory data) = activeGame.maker.call{value: address(this).balance}("");
-        require(sent, "Failed to send Ether");
-        
-      } else {
-          
-        (bool sent, bytes memory data) = activeGame.taker.call{value: address(this).balance}("");
-        require(sent, "Failed to send Ether");
-      }
+        goals[_hash].emailSent = true;
+        activeGoals[_index].emailSent = true;
     }
 
-    function getBalance() public view returns (uint balance) {
-      return address(this).balance;
+    function setGoalSucceeded(uint _index, bytes32 _hash) public onlyOwner {
+        assert(goals[_hash].amount > 0);
+
+        goals[_hash].completed = true;
+        activeGoals[_index].completed = true;
+
+        goals[_hash].owner.transfer(goals[_hash].amount);
+
+        setGoalSucceededEvent(_hash, true);
+    }
+
+    function setGoalFailed(uint _index, bytes32 _hash) public onlyOwner {
+        assert(goals[_hash].amount > 0);
+
+        goals[_hash].completed = false;
+        activeGoals[_index].completed = false;
+
+        owner.transfer(goals[_hash].amount);
+
+        setGoalFailedEvent(_hash, false);
+    }
+
+    function() public payable {}
+
+    function kill() public onlyOwner { 
+        selfdestruct(owner);
     }
 }
